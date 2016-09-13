57ba812c094492455871201cd789590b09e2eb04
Kent C. Dodds <kent@doddsfamily.us>
2016-09-13 15:25:14 -0600

    upgrade react-test-context-provider

--- app-final/components/Button.test.js
+++ app-final/components/Button.test.js
@@ -4,9 +4,8 @@ import getContextProvider from 'react-test-context-provider'
 import Button from './Button'
 
 test('styles the button with a background of the context color', () => {
-  const component = renderer.create(
-    getContextProvider(<Button>Click Me</Button>, {color: 'blue'})
-  )
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+  const getElementWithBlueColorContext = getContextProvider({color: 'blue'})
+  const element = getElementWithBlueColorContext(<Button>Click Me</Button>)
+  const component = renderer.create(element)
+  expect(component).toMatchSnapshot()
 })
