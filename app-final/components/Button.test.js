3a433604f6ccc8987dd4f16c741be420768f7dd3
Kent C. Dodds <kent@doddsfamily.us>
2016-09-13 15:48:54 -0600

    update deps

--- app-final/components/Button.test.js
+++ app-final/components/Button.test.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import getContextProvider from 'react-test-context-provider'
+import getElementWithContext from 'react-test-context-provider'
 import Button from './Button'
 
 test('styles the button with a background of the context color', () => {
-  const getElementWithBlueColorContext = getContextProvider({color: 'blue'})
-  const element = getElementWithBlueColorContext(<Button>Click Me</Button>)
-  const component = renderer.create(element)
+  const reactElement = getElementWithContext({color: 'blue'}, <Button>Click Me</Button>)
+  const component = renderer.create(reactElement)
   expect(component).toMatchSnapshot()
 })
