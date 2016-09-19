9e153623214ae56d62e0b40cb68b9283d63fae56
Kent C. Dodds <kent@doddsfamily.us>
2016-09-19 16:35:22 -0600

    remove exercises from the repo. They will be generated with validate

--- exercises/components/Button.test.js
+++
@@ -1,7 +0,0 @@
-// You're going to need react, react-test-renderer, react-stub-context, and ./Button
-
-test('styles the button with a background of the context color', () => {
-  // get a new version of the Button component by using stubContext to stub it with the color blue
-  // render that component
-  // take a snapshot of the result and verify the snapshot
-})
